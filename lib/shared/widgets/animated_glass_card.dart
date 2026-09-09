39d7cc57c77a939ded7726d1a89665ea406e031f
Parin.M <324813304+Parin-M@users.noreply.github.com>
2026-09-09 09:54:47 +0330

    Update animated_glass_card.dart

--- lib/shared/widgets/animated_glass_card.dart
+++ lib/shared/widgets/animated_glass_card.dart
@@ -4,35 +4,30 @@ class AnimatedGlassCard extends StatefulWidget {
   final Widget child;
   final VoidCallback? onTap;
   const AnimatedGlassCard({super.key, required this.child, this.onTap});
-  @override
-  State<AnimatedGlassCard> createState() => _AnimatedGlassCardState();
+  @override State<AnimatedGlassCard> createState() => _AnimatedGlassCardState();
 }
 class _AnimatedGlassCardState extends State<AnimatedGlassCard> with SingleTickerProviderStateMixin {
-  late final AnimationController _c = AnimationController(vsync: this, duration: const Duration(milliseconds: 700))..repeat(reverse: true);
-  @override
-  void dispose() { _c.dispose(); super.dispose(); }
-  @override
-  Widget build(BuildContext context) {
+  late final AnimationController _c = AnimationController(vsync: this, duration: const Duration(milliseconds: 900))..repeat(reverse: true);
+  @override void dispose() { _c.dispose(); super.dispose(); }
+  @override Widget build(BuildContext context) {
+    final primary = Theme.of(context).colorScheme.primary;
     return AnimatedBuilder(
       animation: _c,
       builder: (_, child) => Transform.translate(
-        offset: Offset(0, -1.5 * _c.value),
+        offset: Offset(0, -1.2 * _c.value),
         child: InkWell(
-          borderRadius: BorderRadius.circular(24),
-          onTap: widget.onTap,
+          borderRadius: BorderRadius.circular(24), onTap: widget.onTap,
           child: Container(
             padding: const EdgeInsets.all(18),
             decoration: BoxDecoration(
-              color: Colors.white.withOpacity(.055),
+              color: Theme.of(context).brightness == Brightness.light ? Colors.white.withOpacity(.66) : Colors.white.withOpacity(.055),
               borderRadius: BorderRadius.circular(24),
-              border: Border.all(color: Colors.white.withOpacity(.09)),
-              boxShadow: [BoxShadow(color: const Color(0xFF6C63FF).withOpacity(.05 + .08 * _c.value), blurRadius: 28, spreadRadius: 1)],
-            ),
-            child: child,
+              border: Border.all(color: primary.withOpacity(.12 + .04 * _c.value)),
+              boxShadow: [BoxShadow(color: primary.withOpacity(.07 + .05 * _c.value), blurRadius: 26, spreadRadius: 1)],
+            ), child: child,
           ),
         ),
-      ),
-      child: widget.child,
+      ), child: widget.child,
     );
   }
 }
@@ -40,12 +35,8 @@ class _AnimatedGlassCardState extends State<AnimatedGlassCard> with SingleTicker
 class PageReveal extends StatelessWidget {
   final Widget child;
   const PageReveal({super.key, required this.child});
-  @override
-  Widget build(BuildContext context) => TweenAnimationBuilder<double>(
-        tween: Tween(begin: 0, end: 1),
-        duration: const Duration(milliseconds: 450),
-        curve: Curves.easeOutCubic,
-        builder: (_, value, child) => Opacity(opacity: value, child: Transform.translate(offset: Offset(0, 18 * (1 - value)), child: child)),
-        child: child,
-      );
+  @override Widget build(BuildContext context) => TweenAnimationBuilder<double>(
+    tween: Tween(begin: 0, end: 1), duration: const Duration(milliseconds: 450), curve: Curves.easeOutCubic,
+    builder: (_, value, child) => Opacity(opacity: value, child: Transform.translate(offset: Offset(0, 18 * (1 - value)), child: child)), child: child,
+  );
 }
